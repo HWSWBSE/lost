3393f3da4c1843429f22ceda9fe298bd3598ed4f
HWSWBSE <godle.doodle.suck@gmail.com>
2026-04-27 23:49:38 +0500

    Add files via upload

---
+++ Mine 1.js
@@ -0,0 +1,31 @@
+const IMAGE_SRCS = [
+
+  "https://raw.githack.com/HWSWBSE/lost/HWSWBSE-patch-cube/%D0%90%D0%90%D0%90%D0%90%D0%94.png",
+
+  "https://raw.githack.com/HWSWBSE/lost/HWSWBSE-patch-cube/%D0%B4%D0%B6%D0%B0%D1%80%D0%B0%D1%85%D0%BE%D0%B2.png",
+
+  "https://raw.githack.com/HWSWBSE/lost/HWSWBSE-patch-cube/%D0%B4%D1%83%D1%83%D1%83%D1%83%D0%B1.png",
+
+  "https://raw.githack.com/HWSWBSE/lost/HWSWBSE-patch-cube/%D0%BA%D0%B0%D0%BC%D0%B5%D0%BD%D1%8C.png",
+
+  "https://raw.githack.com/HWSWBSE/lost/HWSWBSE-patch-cube/%D0%BB%D0%B0%D0%B7%D1%83%D1%80%D0%B8%D1%82.png",
+
+  "https://raw.githack.com/HWSWBSE/lost/HWSWBSE-patch-cube/%D1%80%D0%B5%D0%B4%D1%81%D1%82%D0%BE%D1%83%D0%BD.png"
+
+];
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
+
+  "Obsidian",
+
+  "Wool",
+
+  "Oak",
+
+  "Stone",
+
+  "Lapis Lazuli Ore",
+
+  "Redstone Ore"
+
+];
